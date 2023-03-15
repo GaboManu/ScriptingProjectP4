0eeded415eb3aeadaf08122147279a6b9e3b8bbd
GaboManu <715651@student.dusd.net>
2023-03-15 12:03:40 -0700

    added get component scriopt

---
+++ scriptingtprojectVargas/Assets/Scripts/UsingOtherComponents.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class UsingOtherComponents : MonoBehaviour
+{
+    public GameObject otherGameObject;
+
+
+    private AnotherScript anotherScript;
+    private YetAnotherScript yetAnotherScript;
+    private BoxCollider boxcol;
+
+
+    void Awake()
+    {
+        anotherScript = GetComponent<AnotherScript>();
+        yetAnotherScript = otherGameObject.GetComponent<YetAnotherScript>();
+        boxcol = otherGameObject.GetComponent<BoxCollider>();
+    }
+
+
+    void Start()
+    {
+        boxcol.size = new Vector3(3, 3, 3);
+        Debug.Log("The player's score is " + anotherScript.playerScore);
+        Debug.Log("The Player has died " + yetAnotherScript.numberofPlayerDeaths + " times");
+    }
+}
