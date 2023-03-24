315992ce57ab53a9819b91d1e9ad048ad06b30f0
GaboManu <715651@student.dusd.net>
2023-03-24 11:40:43 -0700

    Made sam_a_flang

---
+++ scriptingtprojectVargas/Assets/Scripts/DataTypeScript.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataTypeScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 pos = transform.position;
+        pos = new Vector3(3, 2, 0);
+
+
+        Transform tran = transform;
+        tran.position = new Vector3(3, 2, 0);
+    }
+}
